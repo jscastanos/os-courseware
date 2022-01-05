90b4e493c1f9206f1739d049800185ddbc2ddced
Jec Castanos <jec.castanos@gmail.com>
2022-01-05 22:49:43 +0800

    fix: accordion readOnly

--- pages/components/Accordion.tsx
+++ pages/components/Accordion.tsx
@@ -67,14 +67,14 @@ export default function Accordion({
       >
         <p className="text-gray-400 text-justify">
           <div className="flex flex-col">
-            {items.map(({ title, time, url }: any) => (
+            {items.map(({ title, time }: any) => (
               <LessonItem
                 key={title}
                 topic={topic}
                 title={title}
                 time={time}
                 active={activeTitle === title && currentTopic}
-                url={url}
+                readOnly={false}
               />
             ))}
           </div>
